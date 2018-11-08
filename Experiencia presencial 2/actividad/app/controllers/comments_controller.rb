4fbb32a74702af29a562837abf827656bfbd538e
Sebastian <blink182seba@gmail.com>
2018-11-08 14:36:17 -0300

    cancancan

---
+++ Experiencia presencial 2/actividad/app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+
+	def create
+		@post = Post.find(params[:post_id])
+		@comment = Comment.new(content:params[:comment][:content], user: current_user)
+		authorize! :create, @commet
+		@post.comments << @comment
+		@comment.save
+		redirect_to @post
+	end
+
+	def destroy
+
+		@post = Post.find(params[:post_id])
+		@comment = Comment.find(params[:id])
+		
+		@comment.destroy
+		redirect_to @post
+	end
+end
